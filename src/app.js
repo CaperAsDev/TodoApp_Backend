36dec8d3a626e80f7f3f4b4a0347b588d0d42fde
Capicode <caper.asdev@gmail.com>
2025-04-18 09:26:18 -0500

    cookies problem fixed, next test in on other endpoints

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ const createApp = async ({ models }) => {
   const app = express()
   app.disable('x-powered-by')
 
-  app.use(cors())
+  app.use(cors({
+    origin: process.env.CORS_ORIGIN ?? 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }))
   app.use(express.urlencoded({ extended: true }))
   app.use(express.json())
   app.use(cookieParser())
